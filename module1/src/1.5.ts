a411dd80f13486ecdc3e7426075f4cc053589515
ProgrammerShahJalal <you@example.com>
2024-05-01 20:03:45 +0600

    object type

---
+++ module1/src/1.5.ts
@@ -0,0 +1,15 @@
+// Reference type --> Object
+
+const user:{
+    religion: 'Islam' // type --> literal types
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+    age: Number;
+} = {
+    religion: 'Islam',
+    firstName: 'Md',
+    middleName: 'Shah',
+    lastName: 'Jalal',
+    age: 25,
+}
