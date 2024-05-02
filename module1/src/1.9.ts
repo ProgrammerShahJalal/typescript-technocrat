c558b01f7b34f63020ceeba5a1dca008eefe8f75
ProgrammerShahJalal <you@example.com>
2024-05-02 09:36:05 +0600

    type alias fn

--- module1/src/1.9.ts
+++ module1/src/1.9.ts
@@ -1,3 +1,6 @@
+{
+
+
 // type Alias
 
 type Student = {
@@ -22,4 +25,15 @@ const student2: Student & {
     hobby: ["Reading","Coding"],
     contact: '1234567890', 
     address: "Shanghai",
+}
+/* ====Type Alias in Function====== */
+type Add = (a:number, b:number) => number;
+const  add:Add =(num1, num2)  => num1 + num2;
+
+
+
+
+
+
+
 }
